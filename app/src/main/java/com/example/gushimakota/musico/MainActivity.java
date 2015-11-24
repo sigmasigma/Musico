f79168aef03fd033df020d7a524c2af7b16f3947
gushimakota <gushi_g.and.v@i.softbank.jp>
2015-11-24 15:59:00 +0900

    next step is spinner implement.

--- app/src/main/java/com/example/gushimakota/musico/MainActivity.java
+++ app/src/main/java/com/example/gushimakota/musico/MainActivity.java
@@ -6,13 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.parse.Parse;
 import com.parse.ParseObject;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     private SeekBar progressBar;
     private MediaPlayer mediaPlayer;
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private Handler handler;
     private int timeProgress;
     private boolean startBool = false;
+    private boolean runBool = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +34,13 @@ public class MainActivity extends AppCompatActivity {
         handler = new Handler();
 
         // Enable Local Datastore.
-        Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "XB4hSQBay6VfK6ZRXTWVh3ir375cML7TOwXgt9mv", "EO4ZV6k2ZWxyiLqJj7LO9YxtAAbHwPrIKsH4LDG5");
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
+//        Parse.enableLocalDatastore(this);
+//        Parse.initialize(this, "XB4hSQBay6VfK6ZRXTWVh3ir375cML7TOwXgt9mv", "EO4ZV6k2ZWxyiLqJj7LO9YxtAAbHwPrIKsH4LDG5");
+//        ParseObject testObject = new ParseObject("TestObject");
+//        testObject.put("foo", "bar");
+//        testObject.saveInBackground();
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.emergence);
+        mediaPlayer = MediaPlayer.create(this, R.raw.musico_pro);
 
 
         progressBar = (SeekBar) findViewById(R.id.seekBar);
@@ -49,30 +53,38 @@ public class MainActivity extends AppCompatActivity {
         btStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try{
-                    mediaPlayer.prepare();
-                    startBool = true;
-                }catch (Exception e){
+                if (mediaPlayer.isPlaying()) {
+                    btStart.setText("Start");
+                    mediaPlayer.pause();
+                    runBool = false;
+                }else{
+
+                    btStart.setText("Pause");
+                    if (startBool == false || timeProgress == mediaPlayer.getDuration()) {
+                        try {
+                            timeProgress = 0;
+                            progressBar.setProgress(0);
+                            mediaPlayer.prepare();
+                        } catch (Exception e) {
+                            startBool = true;
+                            runBool = true;
+                            mediaPlayer.seekTo(timeProgress);
+                            mediaPlayer.start();
+                            runTimeBar();
+                        }
+                    }
                     mediaPlayer.start();
-                    runTimeBar();
+                    runBool = true;
                 }
             }
         });
+
         btStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mediaPlayer.pause();
 
             }
         });
-
-        try{
-            mediaPlayer.prepare();
-            startBool = true;
-        }catch (Exception e){
-            mediaPlayer.start();
-            runTimeBar();
-        }
 
         progressBar.setOnSeekBarChangeListener(
                 new SeekBar.OnSeekBarChangeListener() {
@@ -83,32 +95,56 @@ public class MainActivity extends AppCompatActivity {
 
                     public void onStartTrackingTouch(SeekBar seekBar) {
                         // ツマミに触れたときに呼ばれる
+                        runBool = false;
                     }
 
                     public void onStopTrackingTouch(SeekBar seekBar) {
                         // ツマミを離したときに呼ばれる
-                        if(startBool==false){
-                            try{
-                                mediaPlayer.prepare();
+                        if (startBool == false) {
+                            try {
                                 startBool = true;
-                            }catch (Exception e){
-                                mediaPlayer.seekTo(timeProgress);
-                                mediaPlayer.start();
-                                runTimeBar();
+                                runBool = true;
+                            } catch (Exception e) {
                             }
                         }
+                        btStart.setText("Pause");
                         mediaPlayer.seekTo(timeProgress);
                         mediaPlayer.start();
+                        runTimeBar();
+                        runBool = true;
                     }
                 }
         );
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // アイテムを追加します
+        adapter.add("red");
+        adapter.add("green");
+        adapter.add("blue");
+        Spinner spinner = (Spinner) findViewById(R.id.spinner2);
+        // アダプターを設定します
+        spinner.setAdapter(adapter);
+        // スピナーのアイテムが選択された時に呼び出されるコールバックリスナーを登録します
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Spinner spinner = (Spinner) parent;
+                // 選択されたアイテムを取得します
+                String item = (String) spinner.getSelectedItem();
+                Toast.makeText(MainActivity.this, item, Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
     }
 
     private void runTimeBar(){
         Thread t = new Thread() {
             @Override
             public void run() {
-                while (mediaPlayer.getDuration() - mediaPlayer.getCurrentPosition() >= 0) {
+                while (runBool&&mediaPlayer.getDuration() - mediaPlayer.getCurrentPosition() >= 1) {
                     handler.post(new Runnable() {
                         @Override
                         public void run() {

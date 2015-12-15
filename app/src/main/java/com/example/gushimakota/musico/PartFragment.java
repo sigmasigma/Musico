43b89bff36bb46a048ff26edc74f7d9840605356
gushimakota <gushi_g.and.v@i.softbank.jp>
2015-12-15 20:46:23 +0900

    フラグメントは諦めた。SelectActivityを軽く実装し終わってるところ。

--- app/src/main/java/com/example/gushimakota/musico/PartFragment.java
+++ app/src/main/java/com/example/gushimakota/musico/PartFragment.java
@@ -1,22 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//使わない・。・
+
+
+
+
+
+
+
+
 package com.example.gushimakota.musico;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PartFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PartFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PartFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,7 +44,7 @@ public class PartFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    private ImageView imageView;
 
     public PartFragment() {
         // Required empty public constructor
@@ -64,45 +81,21 @@ public class PartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_part, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void onStart(){
+        super.onStart();
+//        imageView = (ImageView) getActivity().findViewById(R.id.imageView);
+//        imageView.setMaxHeight(imageView.getWidth());
+//        imageView.setImageResource(R.drawable.gragh2);
+//        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    public void setImageView(int resId){
+        imageView = (ImageView) getActivity().findViewById(R.id.imageView);
+        imageView.setImageResource(resId);
     }
 }
--- app/src/main/java/com/example/gushimakota/musico/SelectActivity.java
+++ app/src/main/java/com/example/gushimakota/musico/SelectActivity.java
@@ -1,13 +1,32 @@
 package com.example.gushimakota.musico;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class SelectActivity extends AppCompatActivity {
+
+    private ImageView imageA;
+    private ImageView imageB;
+    private ImageView imageC;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+        imageA = (ImageView)findViewById(R.id.imageAPart);
+        imageB = (ImageView)findViewById(R.id.imageBPart);
+        imageC = (ImageView)findViewById(R.id.imageCPart);
+
+        changeTheProgressimage();
+
+
+    }
+
+    private void changeTheProgressimage(){
+        imageA.setImageResource(R.drawable.gragh2);
+        imageB.setImageResource(R.drawable.gragh3);
+        imageC.setImageResource(R.drawable.gragh4);
     }
 }
